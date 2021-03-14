5a3e8851e96104c0aca6798844d52ed37a898c9c
Xeont88 <xeont88@gmail.com>
2021-03-14 10:40:53 +0300

    first commit.

---
+++ client_tic_tac_toe.py
@@ -0,0 +1,300 @@
+#== client_tic_tac_toe.py     R.R.Sonkin 10.02.2020
+##-- клиент для сетевого варианта игры "Крестики-Нолики"
+##-- часть 0 - интерфейс
+
+
+#== подключить библиотеки
+from tkinter import *
+from tkinter import ttk
+
+# библиотеки для связи
+import threading
+from time import *
+from socket import *
+
+
+#== параметры и функции
+
+#== вывести строку в текстовое поле
+def disp_msg(msg):
+    txt_serv.insert(END, msg + '\n')
+    txt_serv.yview(END)
+
+
+# Параметры и функции работы с сетью
+server_address = ('localhost', 5403)
+# server_address = ('192.168.0.101', 5401)
+mode = 'disconnect'                 # режим работы клиента
+main_tau = 20                       # время цикла главной программы в мс
+lst_in = []                         # очередь для принятых сообщений
+busy_in = 0                         # признак занятости очереди
+ch_name = {'z': 'нолик', 'k': 'крестик'}        # имя значка
+
+# Отправить сообщение по TCP
+def send_msg(msg):
+    client = socket(AF_INET, SOCK_STREAM)
+    bin_msg = bytes(msg, 'UTF-8')
+    try:
+        client.connect(server_address)
+        client.sendall(bin_msg)
+        # res = 'OK'
+        res = ''
+    except:
+        # res = 'not connected'
+        res = 'Сервер выключен'
+    finally:
+        client.close()
+    return res
+
+
+# Аккуратно записать сообщ в очередь сообщений
+def put_msg(msg):
+    global lst_in
+    global busy_in
+    while busy_in:
+        sleep(0.001)
+    busy_in = 1
+    lst_in.append(msg)
+    busy_in = 0
+
+
+# Функция приема сообщ. (один пакет за раз)
+def work_in():
+    global lst_in
+    global busy_in
+    locserv_addr = ('', 5402)
+    lock_sock = socket(AF_INET, SOCK_STREAM)
+    lock_sock.bind(locserv_addr)                # связать с номером порта локального с
+    lock_sock.listen(5)                         # не более 5 ожидающих запросов
+
+    while True:
+        # ждать и получить сообщ. от игрового сервера...
+        # print('ready')
+        connection, address = lock_sock.accept()
+        # print('connected by', address)
+        bin_data = connection.recv(1024)
+        str_data = bin_data.decode('utf-8')
+        # print(str_data)
+        connection.close()
+
+        # ...записать его в очередь сообщ...
+        put_msg(str_data)
+
+        # ...задержка потока
+        sleep(0.001)
+
+
+# поток приема сообщений
+tr_in = threading.Thread(target=work_in)
+tr_in.daemon = True
+tr_in.start()
+
+
+#== создание интерфейса
+root = Tk()
+frm_color = '#52d73B'                   #-- цвет форма
+btn_width = 20                          #-- ширина кнопок
+
+
+# Новая левая панель                                        ////0////
+pn_game = Frame(root, bg='dark green')
+pn_game.pack(side='left')
+
+# параметры поля                                        ////0////
+cell_size = 24
+x_count = 22
+y_count = 22
+
+# сформировать канву                                        ////0////
+canv_width = x_count * cell_size
+canv_height = y_count * cell_size
+
+canv = Canvas(pn_game, width=canv_width, height=canv_height)
+canv.pack()
+
+# очистка канвы
+def init_canv():
+    canv['bg'] = 'white'
+    canv.delete(ALL)
+    canv.create_rectangle(0, 0, canv_width, canv_height)
+    # клетки
+    for x_num in range(x_count):
+        for y_num in range(y_count):
+            canv.create_line(0, y_num*cell_size, canv_width, y_num*cell_size)
+            canv.create_line(x_num*cell_size, 0, x_num*cell_size, canv_height)
+
+
+# вывести на канву значек (крести при ch='k', или нолик при ch=='z')
+def draw_canv(row, col, ch):
+    x_left = col * cell_size + 3; x_right = x_left + cell_size - 6
+    y_top = row * cell_size + 3; y_bottom = y_top + cell_size - 6
+    if ch == 'z':
+        canv.create_oval(x_left, y_top, x_right, y_bottom, outline='#050', )
+    elif ch == 'k':
+        canv.create_line(x_left, y_top, x_right, y_bottom, fill='blue', )
+        canv.create_line(x_left, y_bottom, x_right, y_top, fill='blue', )
+
+
+# привязать к канве отправку хода
+def send_step(event):
+    # определить координаты
+    col = event.x // cell_size
+    row = event.y // cell_size
+    # сформировать и отправить сообщение о ходе
+    res = send_msg('step|' + str(row) + ',' + str(col))
+    disp_msg(res)
+
+
+canv.bind('<Button-1>', send_step)
+
+
+# правая панель с элементами управления
+pnl_right = Frame(root, width=150, bg=frm_color)
+pnl_right.pack(side='right', fill='both', expand=1)
+
+lbl_head = Label(pnl_right, text='Сетевая игра \n "Угадай число"', bg=frm_color, fg='white')
+lbl_head.grid(row=0, column=0, sticky=W)
+
+lbl_empty = Label(pnl_right, text='   ', bg=frm_color, fg='white')
+lbl_empty.grid(row=1, column=0, sticky=W, padx=10)
+
+lbl_mode = Label(pnl_right, text='Незарегистрирован', bg=frm_color, fg='white')
+lbl_mode.grid(row=2, column=0, sticky=W, padx=10)
+
+
+# Кнопка регистрации
+def fnc_registr():
+    name = edt_strout.get().strip()
+    if len(name) < 1:
+        put_msg('регистрация невозможна \n введите имя в строке ввода.')
+        return
+    str_out = 'registr|' + name
+    res = send_msg(str_out)
+    # print(res)
+    disp_msg(res)
+
+
+btn_reg = ttk.Button(pnl_right, text='Зарегистрировать', width=btn_width, command=fnc_registr)
+btn_reg.grid(row=3, column=0, sticky=E+W, padx=5, pady=5)
+
+
+# Кнопка информации
+def fnc_info():
+    str_out = 'get_info'
+    res = send_msg(str_out)
+    # print(res)
+    disp_msg(res)
+
+
+btn_info = ttk.Button(pnl_right, text='Информация', width=btn_width, command=fnc_info)
+btn_info.grid(row=4, column=0, sticky=E+N, padx=5, pady=5)
+
+
+# метка и поле отправляемой строки
+# lbl_step = Label(pnl_right, text='Ваше число:', bg=frm_color, fg='white')     # ////0////
+lbl_step = Label(pnl_right, text='Ваше имя:', bg=frm_color, fg='white')
+lbl_step.grid(row=7, column=0, sticky=W, padx=10)
+
+
+'''                                                     # ////0////
+def fnc_step(event):
+    str_out = edt_strout.get()
+    if not str_out.isnumeric():
+        put_msg('некорректный ход \n должны быть только цифры')
+        return
+    res = send_msg('step|' + str_out)
+    # print(res)
+    disp_msg(res)
+'''
+
+
+edt_strout = Entry(pnl_right, width=btn_width)
+edt_strout.grid(row=8, column=0, padx=10)
+# edt_strout.bind('<Return>', fnc_step)                 # ////0////
+
+
+'''                                                 # ////0////
+# кнопка отправки хода
+btn_step = ttk.Button(pnl_right, text='Отправить', width=btn_width)
+btn_step.grid(row=9, column=0, sticky=E+N, padx=5, pady=5)
+btn_step.bind('<Button-1>', fnc_step)
+'''
+
+
+# кнопка очистки таблицы игроков                 # ////0////
+def fnc_restart(event):
+    str_out = 'restart'
+    res = send_msg(str_out)
+    disp_msg(res)
+
+btn_restart = ttk.Button(pnl_right, text='Новая игра', width=btn_width)
+btn_restart.grid(row=9, column=0, sticky=E+N, padx=5, pady=5)
+btn_restart.bind('<Button-1>', fnc_restart)
+
+
+#-- панель с сообщениями от сервера
+pnl_bottom = Frame(pnl_right, bg=frm_color)
+# pnl_bottom.pack(side='left', fill='y')
+pnl_bottom.grid(row=10, column=0, sticky=E+N, padx=5, pady=5)
+
+txt_serv = Text(pnl_bottom, width=30, height=20, wrap='word', )
+txt_serv.pack(side='left', fill='y')
+
+sbr_serv = Scrollbar(pnl_bottom, orient='vertical')
+sbr_serv.pack(side='right', fill='y')
+
+sbr_serv.config(command=txt_serv.yview)
+txt_serv.config(yscrollcommand=sbr_serv.set, wrap='word')
+
+
+# главная функция, запускаемая в цикле
+def main():
+    global lst_in
+    global busy_in
+    global mode
+    global x_count                      # ////0////
+    global y_count
+
+    # Проверить и вывести данные из очереди строк
+    if len(lst_in) > 0:
+        while busy_in:
+            sleep(0.001)
+        busy_in = 1
+        str_in = lst_in.pop(0)
+        busy_in = 0
+
+        disp_msg(str_in)
+
+        # Обработать регистрацию
+        lst_loc = str_in.split('|')
+        if lst_loc[0] == 'registr':
+            lbl_mode['text'] = lst_loc[1] + ' ( ' + ch_name[lst_loc[2]] + ' )'
+
+        # Обработать ход                ////0////
+        if lst_loc[0] == 'step':
+            row, col, ch = lst_loc[1].split(',')
+            draw_canv(int(row), int(col), ch)
+
+        # Обработать начало игры        ////0////
+        if lst_loc[0] == 'begin':
+            y_count = int(lst_loc[1])
+            x_count = int(lst_loc[2])
+            init_canv()
+
+        # Обработать окончание игры     ////0////
+        if lst_loc[0] == 'end':
+            canv['bg'] = 'yellow'
+
+    # Перезапуститься после задержки
+    root.after(main_tau, main)
+
+
+# запустить главную функцию
+# очистить канву
+canv['bg'] = 'white'
+canv.delete('all')
+canv.create_rectangle(0, 0, canv_width, canv_height)
+
+main()
+
+root.mainloop()
---
+++ server_tic_tac_toe.py
@@ -0,0 +1,339 @@
+#== server_tic_tac_toe.py     R.R.Sonkin 10.02.2020
+##-- сервер для сетевого варианта игры "Крестики-нолики"
+##-- вариант 0 игровой сервер
+
+
+#== подключить библиотеки
+# import threading
+from socket import *
+
+# from random import randint                ////0////
+
+#                                           ////0////
+flag_test = 1               # тестовый режим для одного игрока
+mode = 'регистрация'        # режим регистрации
+# mode = 'игра'             # режим игры
+play_table = []             # таблица игроков [name, ip, ch]             ////0////
+player_count = 2            # число игроков для игры
+player_num = 0              # номер текущего игрока
+ch_table = ['z', 'k']       # список значков (z - нолик, k - крестик)
+x_count = 22
+y_count = 22
+
+
+# массив результатов ходов
+cells = []
+for y_num in range(y_count):
+    row = []
+    for x_num in range(x_count):
+        row.append(' ')
+    cells.append(row)
+
+
+# secret_num = 100            # задуманное число               ////0////
+
+
+# ///////// ФУНКЦИИ /////////
+def clear_cells():          # очистить таблицу ходов
+    global cells
+    for row in range(y_count):
+        for col in range(x_count):
+            cells[row][col] = ''
+
+
+# проверяет есть ли 5 элементов в одну линию подряд (горизонт, вертик и диагональ)
+def count_dir(row, col, d_row, d_col):          # сосчитать число ch в заданном направлении
+    ch_beg = cells[row][col]
+    if not ch_beg: return 0                     # защита от ошибки
+    row_cur = row + d_row; col_cur = col + d_col; res = 0
+
+    while 0 <= row_cur < y_count and 0 <= col_cur < x_count:
+        if cells[row_cur][col_cur] != ch_beg: break
+        res += 1
+        row_cur += d_row; col_cur += d_col
+    return res
+
+
+# Проверка игры на окончание (нет ли победителя?)
+def test_end(row, col):
+    ch_mid = cells[row][col]
+    print('ch_mid =', ch_mid)
+
+    # проверить по вертикали,
+    # сосчитать по вертикали вверх
+    num_up = count_dir(row, col, -1, 0)
+    # сосчитать по вертикали вниз
+    num_down = count_dir(row, col, 1, 0)
+    print('vert', num_up + num_down + 1)
+    if num_up + num_down + 1 >= 5: return 1
+
+    # проверить по горизонтали,
+    # сосчитать по горизонтали влево
+    num_left = count_dir(row, col, 0, -1)
+    # сосчитать по горизонтали вправо
+    num_right = count_dir(row, col, 0, 1)
+    print('horiz', num_left + num_right + 1)
+    if num_left + num_right + 1 >= 5: return 1
+
+    # проверить по одной диагонали,
+    # сосчитать по диагонали влево вверх
+    num_lu = count_dir(row, col, -1, -1)
+    # сосчитать по диагонали вправо вниз
+    num_rd = count_dir(row, col, 1, 1)
+    print('dia_1', num_lu + num_rd + 1)
+    if num_lu + num_rd + 1 >= 5: return 1
+
+    # проверить по второй диагонали,
+    # сосчитать по диагонали влево вниз
+    num_ld = count_dir(row, col, 1, -1)
+    # сосчитать по диагонали вправо вверх
+    num_ru = count_dir(row, col, -1, 1)
+    print('dia_2', num_ld + num_ru + 1)
+    if num_ld + num_ru + 1 >= 5: return 1
+
+    return 0
+
+
+def str_playcount():        # склонение слова игроки
+    if player_count == 1:
+        return '1 игрок'
+    elif player_count < 5:
+        return str(player_count) + ' игрока'
+    else:
+        return str(player_count) + ' игроков'
+
+
+def send_to_all(msg):       # отправить строку всем игрокам
+    for el in play_table:
+        ip_el = el[1]
+        res = send_answ(ip_el, msg)
+
+
+mainserv_addr = ('', 5403)  # порт сервера ивеличен на 2
+main_sock = socket(AF_INET, SOCK_STREAM)
+main_sock.bind(mainserv_addr)                # связать с номером порта локального сервера
+main_sock.listen(5)                         # не более 5 ожидающих запросов
+print('main server works')
+
+
+# отправить строку сообщения по заданному адресу
+def send_answ(ip, msg):
+    client = socket(AF_INET, SOCK_STREAM)
+    bin_msg = bytes(msg, 'UTF-8')
+    try:
+        client.connect((ip, 5402))      # порт клиента увеличен на 2
+        client.send(bin_msg)
+        res = 'OK'
+    except:
+        res = 'not connected'
+    finally:
+        client.close()
+    return res
+
+
+# Главный цикл
+while True:
+
+    print('ready')
+    connection, address = main_sock.accept()          # ждать и получить сообщение от игрока
+    print('connected by', address)
+    bin_data = connection.recv(1024)
+    str_data = bin_data.decode('utf-8')
+    print(str_data)
+    connection.close()
+
+    # обработка принятого сообщения          //// 0 ////
+
+    # выделить адрес клиента
+    ip_client = address[0]
+    print(ip_client)
+
+    # выделить команду и параметр
+    lst_data = str_data.split('|')
+    command = lst_data[0]
+    print(command)
+    try:
+        param = lst_data[1]
+    except:
+        param = ''
+    print('command = ' + command +', param = '+ param + ', mode = '+ mode)
+
+    # отработать команду
+    if command == 'get_info':
+        str_answer = 'состояние сервера\n режим:  ' + mode
+        str_answer += '\n список игроков: '
+        for el in play_table:
+            str_answer += '\n' + el[0] + ', ip = ' + el[1]
+        if mode == 'игра':
+            str_answer += '\n ходит: ' + play_table[player_num][0]
+        elif mode == 'регистрация':
+            str_answer += '\n необходимо: ' + str_playcount()
+        res = send_answ(ip_client, str_answer)
+
+    elif command == 'registr':
+        if mode == 'игра':
+            str_answer = '\n во время игры регистрация запрещена '
+            res = send_answ(ip_client, str_answer)
+            continue
+        # зарегистрировать игрока
+        # проверить игрока по ip адресу в таблице
+        flag_find = 0
+        for el in play_table:
+            if el[1] == ip_client:
+                el[0] = param
+                flag_find = 1
+                break
+
+        # добавить игрока в таблицу, если его там нет (flag_find==0)
+        if flag_find == 0:
+            ch = ch_table[len(play_table)]                  # ////0////
+            play_table.append([param, ip_client, ch])
+
+            if flag_test:       # в тестовом режиме сдублировать игрока    ////0////
+                ch = ch_table[len(play_table)]
+                play_table.append([param, ip_client, ch])
+        clear_cells()                                       # ////0////
+        player_num = 0
+
+        # подтвердить регистрацию
+        str_answer = str_data + '|' + ch                    # ////0////
+        res = send_answ(ip_client, str_answer)
+
+        # сообщить всем о регистрации игрока
+        str_answer = '\n Зарегистрирован игрок: ' + param
+        send_to_all(str_answer)
+
+        # начать игру, если игроков достаточно
+        if len(play_table) >= player_count:
+            # начальные установки в игре
+            # secret_num = randint(1, secret_num)           # ////0////
+            clear_cells()
+            player_num = 0
+
+            # сообщить всем о начале игры                   # ////0////
+            gamer_name = play_table[player_num][0]
+            str_answer = 'begin|' + str(y_count) + '|' + \
+                         str(x_count) + '|' + ' игра началась!\nходит: ' + gamer_name + '\n'
+            # str_answer = '\n Игра началась \n задумано число от 1 до {}'.format(secret_num)
+            send_to_all(str_answer)
+
+            # дать ход первому зарегистрированному
+            ip_el = play_table[player_num][1]
+            res = send_answ(ip_el, '\n Ваш ход')
+
+            # переключиться в режим игры
+            mode = 'игра'
+            continue
+
+    elif command == 'step':
+        if mode == 'регистрация':
+            str_answer = '\n во время регистрации запрещено ходить '
+            res = send_answ(ip_client, str_answer)
+            continue
+
+        # проверить игрока по ip адресу
+        el = play_table[player_num]
+        if el[1] != ip_client:
+            str_answer = '\n сейчас не Ваш ход =)'
+            res = send_answ(ip_client, str_answer)
+            continue
+
+        # обработать ход
+        '''
+        # проверить корректность хода
+        if not param.isnumeric():
+            str_answer = '\n ваш ход не корректен, \n нужно отправить целое число'
+            res = send_answ(ip_client, str_answer)
+            continue
+
+        # извлечь значение хода
+        try:
+            int_param = int(param)
+        except:
+            int_param = 0
+
+        # сравнить с задуманным числом
+        if secret_num > int_param:
+            str_res = 'больше'
+        elif secret_num < int_param:
+            str_res = 'меньше'
+        else:
+            str_res = 'угадал'
+        '''
+
+        try:
+            str_row, str_col = param.split(',')
+            row = int(str_row)
+            col = int(str_col)
+        except:
+            row, col = 0, 0
+
+        if row >= y_count: row = y_count - 1
+        if col >= x_count: col = x_count - 1
+        ch = play_table[player_num][2]
+        if cells[row][col] != '':
+            str_answer = 'Ваш ход некорректен \n'
+            res = send_answ(ip_client, str_answer)
+            continue
+
+        # сообщишь всем о ходе                  # ////0////
+        cells[row][col] = ch
+        gamer = play_table[player_num]
+        gamer_name = gamer[0]
+        gamer_ip = gamer[1]
+        # str_answer = '\n ' + gamer_name + ': ' + param + ' - ' + str_res
+        str_answer = str_data + ',' + ch + '|' + gamer_name + '\n'
+
+        send_to_all(str_answer)
+
+        # закончить игру, если число угадано
+        '''
+        if str_res == 'угадал':
+            str_answer = '\n ' + gamer_name + ' выиграл, \n Игра окончена!'
+            send_to_all(str_answer)
+            mode = 'регистрация'
+            continue
+        '''
+
+        # проверяем ...
+        res = test_end(row, col)
+        if res == 1:
+            str_answer = 'end|' + gamer_name + ' выиграл, \n игра окончена'
+            send_to_all(str_answer)
+            mode = 'регистрация'
+            continue
+
+        # передать ход при неправильном ответе
+        player_num += 1
+        if player_num >= len(play_table): player_num = 0
+
+        ip_el = play_table[player_num][1]
+        res = send_answ(ip_el, '\n Ваш ход')
+
+    elif command == 'restart':
+        if mode == 'игра':
+            str_answer = 'во время игры restart запрещен'
+            res = send_answ(ip_client, str_answer)
+            continue
+
+        str_answer = 'таблица игроков очищена. \n Перерегистрируйтесь'
+        send_to_all(str_answer)
+
+        # очистить таблицу игроков и массив ходов
+        play_table = []
+        clear_cells()
+        player_num = 0
+
+        continue
+
+
+
+    else:                                           # недопустимая команда
+        str_answer = '\n ошибочная команда серверу '
+        res = send_answ(ip_client, str_answer)
+
+
+
+
+
+
